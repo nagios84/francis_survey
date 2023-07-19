d08ee8e02b8017a12225968e28035a497fe84762
Yahya Torshho <nagios84@gmail.com>
2023-07-19 11:05:57 +0300

    First simple changes

--- asdf.js
+++ asdf.js
@@ -11,6 +11,38 @@ createApp({
         id: 2,
         title: 'Российское национальное телевидение',
         model: null
+      }, {
+        id: 3,
+        title: 'YouTube',
+        model: null
+      }, {
+        id: 4,
+        title: 'Vkontakte/Odnoklassniki',
+        model: null
+      }, {
+        id: 5,
+        title: 'Telegram',
+        model: null
+      }, {
+        id: 6,
+        title: 'Видеоигры',
+        model: null
+      }, {
+        id: 7,
+        title: 'Радио',
+        model: null
+      }, {
+        id: 8,
+        title: 'Международное телевидение',
+        model: null
+      }, {
+        id: 9,
+        title: 'Местные новостные сайты',
+        model: null
+      }, {
+        id: 10,
+        title: 'Instagram',
+        model: null
       },
       // сюда добавить все варианты, id - УНИКАЛЬНОЕ число
     ])
